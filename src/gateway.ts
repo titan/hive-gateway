2ec2e106714bd6c0b011d019e2099688398301f9
Titan <howay.tan@gmail.com>
2016-09-05 18:12:17 +0800

    Use fixed routes

--- src/gateway.ts
+++ src/gateway.ts
@@ -1,9 +1,9 @@
-import * as monitor from 'node-docker-monitor';
 import * as http from 'http';
 import * as msgpack from 'msgpack-lite';
 import * as nanomsg from 'nanomsg';
 import * as bunyan from 'bunyan';
 import * as Redis from 'redis';
+import * as hostmap from './hostmap';
 
 let log = bunyan.createLogger({
   name: 'gateway',
@@ -25,88 +25,9 @@ let log = bunyan.createLogger({
   ]
 });
 
-let routes = {}; // {mod: {docker-id: addr}}
-let counters = {}; // {mod: integer}
+let routes = hostmap.default;
 
 let redis = Redis.createClient(6379, process.env['CACHE_HOST']);
-
-var dockerOpts = null;
-
-if (!process.env.DOCKER_SOCKET) {
-  if (!process.env.Docker_HOST) {
-    dockerOpts = {
-      socketPath: '/var/run/docker.sock'
-    };
-  } else {
-    dockerOpts = {
-      host: process.env.DOCKER_HOST,
-      port: process.env.DOCKER_PORT
-    }
-  }
-} else {
-  dockerOpts = {
-    socketPath: process.env.DOCKER_SOCKET 
-  }
-}
-
-monitor({
-  onContainerUp: function (containerInfo, docker) {
-    if (containerInfo.Labels && containerInfo.Labels.api_module) {
-      // register a new route if container has "api_module" label defined
-      let container = docker.getContainer(containerInfo.Id);
-      // get running container details
-      container.inspect(null, (err, containerDetails) => {
-        if (err) {
-          log.error(err, 'Error getting container details for: %j', containerInfo);
-        } else {
-          let mod = containerInfo.Labels.api_module;
-          try {
-            // prepare and register a new route
-            let addr = getUpstreamAddress(containerDetails);
-
-            if (addr) {
-              if (routes[mod]) {
-                routes[mod][containerInfo.Id] = addr;
-              } else {
-                let route = {}
-                route[containerInfo.Id] = addr;
-                routes[mod] = route;
-                counters[mod] = 0;
-              }
-
-              log.info('Registered new api route: %s => %s', mod, addr);
-            } else {
-              log.error('Cannot register api route to mod %s with null address', mod);
-            }
-          } catch (e) {
-            log.error(e, 'Error creating new api route for: %j', containerDetails);
-          }
-        }
-      });
-    }
-  },
-
-  onContainerDown: function (container, docker) {
-    if (container.Labels && container.Labels.api_module) {
-      // remove existing route when container goes down
-      let mod = container.Labels.api_module;
-      let route = routes[mod];
-      if (route) {
-        let addr = route[container.Id];
-        if (addr) {
-          delete route[container.Id];
-          log.info('Removed api route: %s => %s', mod, addr);
-        }
-      }
-    }
-  },
-
-  onMonitorStarted: function (monitor, docker) {
-  },
-
-  onMonitorStopped: function (monitor, docker) {
-  }
-}, dockerOpts);
 
 // create and start http server
 let server = http.createServer((req, rep) => {
@@ -169,29 +90,9 @@ let server = http.createServer((req, rep) => {
   }
 });
 
-// generate upstream address from containerDetails
-function getUpstreamAddress(containerDetails) {
-  let ports = containerDetails.NetworkSettings.Ports;
-  for (let id in ports) {
-    if (ports.hasOwnProperty(id)) {
-      if (containerDetails.NetworkSettings.Networks) {
-        for (let attr in containerDetails.NetworkSettings.Networks) {
-          if (containerDetails.NetworkSettings.Networks.hasOwnProperty(attr)) {
-            return 'tcp://' + containerDetails.NetworkSettings.Networks[attr].IPAddress + ':' + id.split('/')[0];
-          }
-        }
-      } else {
-        return 'tcp://' + containerDetails.NetworkSettings.IPAddress + ':' + id.split('/')[0];
-      }
-    }
-  }
-  return null;
-}
-
 function call (route, mod, params, rep) {
-  let idx = (counters[mod] ++) % Object.keys(route).length;
-  let addr = route[Object.keys(route)[idx]];
-  log.info({params: params}, 'call %s.%s %s to %s', mod, params.fun, JSON.stringify(params.arg), addr);
+  let addr = route
+  log.info({params: params}, 'call %s.%s %s to %s', mod, params.fun, JSON.stringify(params.args), addr);
   let request = nanomsg.socket('req');
   request.connect(addr);
   request.send(msgpack.encode(params));
