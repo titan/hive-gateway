dd142be960c18e6a7bb7272e1b8f7470eeee8ad4
Titan <howay.tan@gmail.com>
2016-11-02 19:47:54 +0800

    Make tslint happy

--- src/gateway.ts
+++ src/gateway.ts
@@ -1,26 +1,26 @@
-import * as http from 'http';
-import * as msgpack from 'msgpack-lite';
-import * as nanomsg from 'nanomsg';
-import * as bunyan from 'bunyan';
-import * as Redis from 'redis';
-import * as zlib from 'zlib';
-import { servermap } from 'hive-hostmap';
+import * as http from "http";
+import * as msgpack from "msgpack-lite";
+import * as nanomsg from "nanomsg";
+import * as bunyan from "bunyan";
+import * as Redis from "redis";
+import * as zlib from "zlib";
+import { servermap } from "hive-hostmap";
 
 let log = bunyan.createLogger({
-  name: 'gateway',
+  name: "gateway",
   streams: [
     {
-      level: 'info',
-      path: '/var/log/gateway-info.log',  // log ERROR and above to a file
-      type: 'rotating-file',
-      period: '1d',   // daily rotation
+      level: "info",
+      path: "/var/log/gateway-info.log",  // log ERROR and above to a file
+      type: "rotating-file",
+      period: "1d",   // daily rotation
       count: 7        // keep 7 back copies
     },
     {
-      level: 'error',
-      path: '/var/log/gateway-error.log',  // log ERROR and above to a file
-      type: 'rotating-file',
-      period: '1w',   // daily rotation
+      level: "error",
+      path: "/var/log/gateway-error.log",  // log ERROR and above to a file
+      type: "rotating-file",
+      period: "1w",   // daily rotation
       count: 3        // keep 7 back copies
     }
   ]
@@ -28,88 +28,87 @@ let log = bunyan.createLogger({
 
 let routes = servermap;
 
-let redis = Redis.createClient(6379, process.env['CACHE_HOST']);
+let redis = Redis.createClient(6379, process.env["CACHE_HOST"]);
 
 // create and start http server
 let server = http.createServer((req, rep) => {
   if (req.headers.origin) {
-    rep.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    rep.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   } else {
-    rep.setHeader('Access-Control-Allow-Origin', '*');
+    rep.setHeader("Access-Control-Allow-Origin", "*");
   }
-  rep.setHeader('Access-Control-Allow-Credentials', 'true');
-  rep.setHeader('Access-Control-Allow-Methods', 'POST');
+  rep.setHeader("Access-Control-Allow-Credentials", "true");
+  rep.setHeader("Access-Control-Allow-Methods", "POST");
 
-  if (req.method == 'POST') {
+  if (req.method === "POST") {
     let chunks = [];
-    req.on('data', (chunk) => {
+    req.on("data", (chunk) => {
       chunks.push(chunk);
     });
-    req.on('end', () => {
+    req.on("end", () => {
       const data = msgpack.decode(Buffer.concat(chunks));
       const mod = data.mod;
       const fun = data.fun;
       const arg = data.arg;
       const ctx = data.ctx;
 
-      let openid = ctx? ctx.wxuser: null;
+      let openid = ctx ? ctx.wxuser : null;
 
       let route = routes[mod];
 
       if (route) {
-        let uid = '';
+        let uid = "";
         if (openid) {
-          redis.hget('wxuser', openid, (err, reply) => {
+          redis.hget("wxuser", openid, (err, reply) => {
             if (!err) {
               uid = reply;
             }
             let params = {
-              ctx: { domain: 'mobile', ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress, uid: uid },
+              ctx: { domain: "mobile", ip: req.headers["x-forwarded-for"] || req.socket.remoteAddress, uid: uid },
               fun: fun,
               args: arg
             };
-            const acceptEncoding = req.headers['accept-encoding'];
-            call(acceptEncoding ?  acceptEncoding : '', route, mod, params, rep);
+            const acceptEncoding = req.headers["accept-encoding"];
+            call(acceptEncoding ?  acceptEncoding : "", route, mod, params, rep);
           });
         } else {
           let params = {
-            ctx: { domain: 'mobile', ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress, uid: uid },
+            ctx: { domain: "mobile", ip: req.headers["x-forwarded-for"] || req.socket.remoteAddress, uid: uid },
             fun: fun,
             args: arg
           };
-          const acceptEncoding = req.headers['accept-encoding'];
-          call(acceptEncoding ?  acceptEncoding : '', route, mod, params, rep);
+          const acceptEncoding = req.headers["accept-encoding"];
+          call(acceptEncoding ?  acceptEncoding : "", route, mod, params, rep);
         }
       } else {
-        log.info('%s.%s %s not found', mod, fun, JSON.stringify(arg));
-        rep.writeHead(404, {'Content-Type': 'text/plain'});
-        rep.end('Module not found');
+        log.info("%s.%s %s not found", mod, fun, JSON.stringify(arg));
+        rep.writeHead(404, {"Content-Type": "text/plain"});
+        rep.end("Module not found");
       }
     });
   } else {
-    log.info('%s %s invalid rpc call', req.method, req.url);
-    rep.writeHead(405, {'Content-Type': 'text/plain'});
-    rep.end('Invalid rpc call');
+    log.info("%s %s invalid rpc call", req.method, req.url);
+    rep.writeHead(405, {"Content-Type": "text/plain"});
+    rep.end("Invalid rpc call");
   }
 });
 
-function call (acceptEncoding, route, mod, params, rep) {
-  let addr = route
-  log.info({params: params}, 'call %s.%s %s to %s', mod, params.fun, JSON.stringify(params.args), addr);
-  let request = nanomsg.socket('req');
+function call (acceptEncoding, addr, mod, params, rep) {
+  log.info({params: params}, "call %s.%s %s to %s", mod, params.fun, JSON.stringify(params.args), addr);
+  let request = nanomsg.socket("req");
   request.connect(addr);
   request.send(msgpack.encode(params));
-  request.on('data', (msg) => {
+  request.on("data", (msg) => {
     if (msg.length > 1024) {
       if (acceptEncoding.match(/\bdeflate\b/)) {
         zlib.deflate(msg, (err: Error, buf: Buffer) => {
           if (err) {
-            rep.writeHead(200, { 'Content-Type': 'application/octet-stream'});
+            rep.writeHead(200, { "Content-Type": "application/octet-stream"});
             rep.write(msg);
             rep.end();
             request.close();
           } else {
-            rep.writeHead(200, { 'Content-Type': 'application/octet-stream', 'Content-Encoding': 'deflate' });
+            rep.writeHead(200, { "Content-Type": "application/octet-stream", "Content-Encoding": "deflate" });
             rep.write(buf);
             rep.end();
             request.close();
@@ -118,25 +117,25 @@ function call (acceptEncoding, route, mod, params, rep) {
       } else if (acceptEncoding.match(/\bgzip\b/)) {
         zlib.gzip(msg, (err: Error, buf: Buffer) => {
           if (err) {
-            rep.writeHead(200, { 'Content-Type': 'application/octet-stream'});
+            rep.writeHead(200, { "Content-Type": "application/octet-stream"});
             rep.write(msg);
             rep.end();
             request.close();
           } else {
-            rep.writeHead(200, { 'Content-Type': 'application/octet-stream', 'Content-Encoding': 'gzip' });
+            rep.writeHead(200, { "Content-Type": "application/octet-stream", "Content-Encoding": "gzip" });
             rep.write(buf);
             rep.end();
             request.close();
           }
         });
       } else {
-        rep.writeHead(200, { 'Content-Type': 'application/octet-stream'});
+        rep.writeHead(200, { "Content-Type": "application/octet-stream"});
         rep.write(msg);
         rep.end();
         request.close();
       }
     } else {
-      rep.writeHead(200, { 'Content-Type': 'application/octet-stream'});
+      rep.writeHead(200, { "Content-Type": "application/octet-stream"});
       rep.write(msg);
       rep.end();
       request.close();
@@ -144,5 +143,5 @@ function call (acceptEncoding, route, mod, params, rep) {
   });
 }
 
-log.info('API gateway is listening on port: 8000');
+log.info("API gateway is listening on port: 8000");
 server.listen(8000);
